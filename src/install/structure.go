79307ab6182ebdbe7d7dcf6fcd2f1b08cdbffc4d
Mateus Souza <mateussouzaweb@gmail.com>
2023-10-06 16:48:23 -0300

    Safer structure installation

--- src/install/structure.go
+++ src/install/structure.go
@@ -11,46 +11,39 @@ import (
 // Ensure folder structure to install programs
 func Structure() error {
 
-	// Check for the presence of games folder, if exist, then is ok
+	// Check for the presence of games folder in home
+	// If exist, then is ok and we can skip
 	info, err := os.Stat(os.ExpandEnv("$HOME/Games"))
 	if !os.IsNotExist(err) && info.IsDir() {
 		return nil
 	}
 
-	// Create base games folder structure
-	err = cli.Command(`
-		mkdir -p $HOME/Games/BIOS
-		mkdir -p $HOME/Games/ROMs
-		mkdir -p $HOME/Games/Save
-	`).Run()
-
-	if err != nil {
-		return err
+	// Check if must install it on microSD
+	// If not, then just create the base games folder structure on home
+	installOnMicroSD := cli.Read("INSTALL_ON_MICROSD", "Install on MicroSD? Y/N", "N")
+	if strings.ToUpper(installOnMicroSD) != "Y" {
+		return cli.Command(`
+			mkdir -p $HOME/Games/BIOS
+			mkdir -p $HOME/Games/ROMs
+			mkdir -p $HOME/Games/Save
+		`).Run()
 	}
 
-	// Check if must install it on microSD, if no, then is ok
-	toMicroSD := cli.Read("INSTALL_TO_MICROSD", "Install to MicroSD? Y/N", "N")
-	if strings.ToUpper(toMicroSD) == "N" {
-		return nil
-	}
-
-	// Get path to microSD
+	// Get microSD path to install on it
 	microSDPath := cli.Read("MICROSD_PATH", "What is the path of the MicroSD?", "/run/media/mmcblk0p1")
 	microSDPath = strings.TrimRight(microSDPath, "/")
 
 	// Make symlinks
 	err = cli.Command(fmt.Sprintf(`
-		# Remove folders in home to create symlink
-		[ -d "$HOME/Games/BIOS" ] && rm -r $HOME/Games/BIOS
-		[ -d "$HOME/Games/ROMs" ] && rm -r $HOME/Games/ROMs
-		[ -d "$HOME/Games/Save" ] && rm -r $HOME/Games/Save
-
 		# Make sure base folders exist on microSD
 		mkdir -p %s/Games/BIOS
 		mkdir -p %s/Games/ROMs
 		mkdir -p %s/Games/Save
 
-		# Create symlinks
+		# Make sure base folder exist on home
+		mkdir -p $HOME/Games
+
+		# Create symlinks on home from microSD folders
 		ln -s %s/Games/BIOS $HOME/Games/BIOS
 		ln -s %s/Games/ROMs $HOME/Games/ROMs
 		ln -s %s/Games/Save $HOME/Games/Save`,
