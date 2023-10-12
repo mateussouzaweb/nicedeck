d7cb275b5f47342b94b0bbedb0cde9db2097b3b9
Mateus Souza <mateussouzaweb@gmail.com>
2023-10-12 02:37:41 -0300

    Better way to write controller templates

--- src/steam/controller/template.go
+++ src/steam/controller/template.go
@@ -3,20 +3,27 @@ package controller
 import (
 	"embed"
 	"os"
+	"path/filepath"
 )
 
 //go:embed resources/*
 var resourcesContent embed.FS
 
-// Save controller template on Steam
-func SaveTemplate(destinationFile string) error {
+// Write controller templates on Steam at given destination path
+func WriteTemplates(destinationPath string) error {
 
+	controllerFile := destinationPath + "/controller_neptune_nicedeck.vdf"
 	controllerConfig, err := resourcesContent.ReadFile("resources/controller.vdf")
 	if err != nil {
 		return err
 	}
 
-	err = os.WriteFile(destinationFile, controllerConfig, 0666)
+	err = os.MkdirAll(filepath.Dir(controllerFile), 0774)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(controllerFile, controllerConfig, 0666)
 	if err != nil {
 		return err
 	}
