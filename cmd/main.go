3818c33484c53b88583e5b9682aa8867e8370fc2
Mateus Souza <mateussouzaweb@gmail.com>
2023-10-12 22:36:17 -0300

    Version 0.0.9

--- cmd/main.go
+++ cmd/main.go
@@ -36,7 +36,7 @@ var installMap = map[string]func() error{
 
 // Version command
 func printVersion() error {
-	cli.Printf(cli.ColorDefault, "Version 0.0.8\n")
+	cli.Printf(cli.ColorDefault, "Version 0.0.9\n")
 	return nil
 }
 
