9a5327d130f2237bed44185bb8b97e9d2f99e637
Mateus Souza <mateussouzaweb@gmail.com>
2023-10-15 19:41:43 -0300

    Skip process to downloader

--- src/steam/downloader.go
+++ src/steam/downloader.go
@@ -1,14 +1,39 @@
 package steam
 
 import (
+	"errors"
 	"net/http"
 	"os"
 	"path/filepath"
 )
 
+// Check if file already exist
+func FileExist(file string) (bool, error) {
+
+	_, err := os.Stat(file)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return false, err
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+
+	return true, nil
+}
+
 // Download file from URL into destination
 func DownloadFile(url string, destinationFile string) error {
 
+	// Check if file exists and if true, skip download process
+	exist, err := FileExist(destinationFile)
+	if err != nil {
+		return err
+	}
+	if exist {
+		return nil
+	}
+
+	// Retrieve file from HTTP
 	response, err := http.Get(url)
 	if err != nil {
 		return err
@@ -16,6 +41,7 @@ func DownloadFile(url string, destinationFile string) error {
 
 	defer response.Body.Close()
 
+	// Make sure file is created and writable
 	err = os.MkdirAll(filepath.Dir(destinationFile), 0774)
 	if err != nil {
 		return err
@@ -28,6 +54,7 @@ func DownloadFile(url string, destinationFile string) error {
 
 	defer file.Close()
 
+	// Write HTTP response body to destination file
 	_, err = file.ReadFrom(response.Body)
 	if err != nil {
 		return err
