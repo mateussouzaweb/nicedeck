80af8464e52b86671447627925dbc11a9b55bca8
Mateus Souza <mateussouzaweb@gmail.com>
2024-02-10 15:17:34 -0300

    Feat: Improved listing with images for programs

--- frontend/scripts/install.js
+++ frontend/scripts/install.js
@@ -13,7 +13,7 @@ window.addEventListener('load', async () => {
                 <input type="checkbox" name="programs[]" value="${program.id}" />
                 <div class="area">
                     <div class="icon">
-                        <img loading="lazy" src="${program.iconUrl}" alt="${program.name}" width="96" height="96" />
+                        <img loading="lazy" src="/img/programs/${program.id}.png" alt="${program.name}" width="96" height="96" />
                     </div>
                     <div class="info">
                         <b>${program.name}</b><br/>
