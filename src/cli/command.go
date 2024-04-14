529151b8ac3544354c8dc027badebf255b4f3116
Mateus Souza <mateussouzaweb@gmail.com>
2024-04-14 00:18:04 -0300

    Feat (cli): ensure flatpak-spawn is not present when program is running outside flatpak

--- src/cli/command.go
+++ src/cli/command.go
@@ -9,11 +9,11 @@ import (
 // Command creates a new command struct
 func Command(script string) *exec.Cmd {
 
+	script = strings.Replace(script, "/usr/bin/flatpak-spawn --host", "", 1)
 	cmd := exec.Command("/bin/bash", "-c", script)
 
 	// Apply flatpak-spawn if application is running inside flatpak
 	if os.Getenv("FLATPAK_ID") != "" {
-		script = strings.Replace(script, "/usr/bin/flatpak-spawn --host", "", 1)
 		cmd = exec.Command("/usr/bin/flatpak-spawn", "--host", "/bin/bash", "-c", script)
 	}
 
