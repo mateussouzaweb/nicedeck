e8ed012f67ad4d541fbe8f2d92182ad371d100ee
Mateus Souza <mateussouzaweb@gmail.com>
2024-04-25 01:25:44 -0300

    Feat (gui): QT mode disabled until we got able to build with flatpack

--- src/gui/gui.go
+++ src/gui/gui.go
@@ -5,16 +5,17 @@ import (
 	"github.com/mateussouzaweb/nicedeck/src/gui/browser"
 	"github.com/mateussouzaweb/nicedeck/src/gui/gtk"
 	"github.com/mateussouzaweb/nicedeck/src/gui/headless"
-	"github.com/mateussouzaweb/nicedeck/src/gui/qt"
 )
 
 // Open UI with best available GUI mode
 func Open(mode string, url string, version string) error {
 
+	// if mode == "qt" {
+	// 	return qt.Open(url, version)
+	// }
+
 	if mode == "gtk" {
 		return gtk.Open(url, version)
-	} else if mode == "qt" {
-		return qt.Open(url, version)
 	} else if mode == "browser" {
 		return browser.Open(url)
 	} else if mode != "headless" {
