559ac0f74484de764fef931f0b813acb3847be5f
Mateus Souza <mateussouzaweb@gmail.com>
2025-02-08 06:19:51 +0000

    Feat: add support to unzip files

---
+++ src/fs/archive.go
@@ -0,0 +1,67 @@
+package fs
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// Unzip given source ZIP file into destination
+func Unzip(source, destination string) error {
+
+	// Open zip file
+	archive, err := zip.OpenReader(source)
+	if err != nil {
+		return err
+	}
+
+	defer archive.Close()
+
+	// Process each item of the archive
+	// When destination already exists
+	// Files outside of the archive will not be removed
+	for _, file := range archive.Reader.File {
+		path := filepath.Join(destination, file.Name)
+
+		// Read directory or file content
+		reader, err := file.Open()
+		if err != nil {
+			return err
+		}
+
+		defer reader.Close()
+
+		// If is a directory, just ensure that the folder exists
+		if file.FileInfo().IsDir() {
+			err = os.MkdirAll(path, os.ModePerm)
+			if err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		// When is file, first remove the existing file
+		err = RemoveFile(path)
+		if err != nil {
+			return err
+		}
+
+		// Now create the target file again
+		writer, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+
+		defer writer.Close()
+
+		// Copy file content to target
+		_, err = io.Copy(writer, reader)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
