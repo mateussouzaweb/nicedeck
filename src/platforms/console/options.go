c781bbe8728d11c7ced7adf669451dc633c564e5
Mateus Souza <mateussouzaweb@gmail.com>
2025-10-14 04:31:13 +0000

    Fix: make ROMs exclude rules works with Windows again

--- src/platforms/console/options.go
+++ src/platforms/console/options.go
@@ -38,16 +38,16 @@ func ToOptions(platforms []string, preferences []string) *Options {
 	// Files with these name patterns will be ignored
 	compile := regexp.MustCompile
 	options.Exclude = []*regexp.Regexp{
-		compile("(?i)[\\/]Updates[\\/]"), // Updates folder
-		compile("(?i)[\\/]Mods[\\/]"),    // Mods folder
-		compile("(?i)[\\/]DLCs[\\/]"),    // DLCs folder
-		compile("(?i)[\\/]Others[\\/]"),  // Folder with games to ignore
-		compile("(?i)[\\/]Ignore[\\/]"),  // Folder with games to ignore
-		compile("(?i)[\\/]Hide[\\/]"),    // Folder with games to ignore
-		compile("(?i)Disc 0?[2-9]"),      // Disc 02 - 09 of some games
-		compile("(?i)Disc [1-9][0-9]"),   // Disc 10 - 99 of some games
-		compile("(?i)Track 0?[1-9]"),     // Track 01 - 09 of some games
-		compile("(?i)Track [1-9][0-9]"),  // Track 10 - 99 of some games
+		compile("(?i)[\\\\/]Updates[\\\\/]"), // Updates folder
+		compile("(?i)[\\\\/]Mods[\\\\/]"),    // Mods folder
+		compile("(?i)[\\\\/]DLCs[\\\\/]"),    // DLCs folder
+		compile("(?i)[\\\\/]Others[\\\\/]"),  // Folder with games to ignore
+		compile("(?i)[\\\\/]Ignore[\\\\/]"),  // Folder with games to ignore
+		compile("(?i)[\\\\/]Hide[\\\\/]"),    // Folder with games to ignore
+		compile("(?i)Disc 0?[2-9]"),          // Disc 02 - 09 of some games
+		compile("(?i)Disc [1-9][0-9]"),       // Disc 10 - 99 of some games
+		compile("(?i)Track 0?[1-9]"),         // Track 01 - 09 of some games
+		compile("(?i)Track [1-9][0-9]"),      // Track 10 - 99 of some games
 	}
 
 	return &options
